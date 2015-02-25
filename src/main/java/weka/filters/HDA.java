9aa10b93fcb18ebb347cfd41e8d9ab7b69faae68
Stephen Nusko <stephen.tkn@gmail.com>
2015-02-25 18:18:48 -0500

    Updated SimpleBatch to construct a matrix of the data points, first step towards HDA implementation

--- src/main/java/weka/filters/HDA.java
+++ src/main/java/weka/filters/HDA.java
@@ -3,10 +3,12 @@ package weka.filters;
 import weka.core.*;
 import weka.core.Capabilities.*;
 import weka.filters.*;
+import weka.core.matrix.*;
 
-public class SimpleBatch
+public class HDA
   extends SimpleBatchFilter {
 
+  private final boolean DEBUG = true;
   public String globalInfo() {
     return   "A simple batch filter that adds an additional attribute 'bla' at the end "
       + "containing the index of the processed instance.";
@@ -27,18 +29,28 @@ public class SimpleBatch
   }
 
   protected Instances process(Instances inst) {
+    double double_matrix[][] = new double[inst.size()][inst.numAttributes()];
+
     Instances result = new Instances(determineOutputFormat(inst), 0);
     for (int i = 0; i < inst.numInstances(); i++) {
       double[] values = new double[result.numAttributes()];
-      for (int n = 0; n < inst.numAttributes(); n++)
+      for (int n = 0; n < inst.numAttributes(); n++) {
         values[n] = inst.instance(i).value(n);
-      values[values.length - 1] = i;
+      }
+      values[values.length - 1] = inst.instance(i).classValue();
+      double_matrix[i] = values;
       result.add(new DenseInstance(1, values));
+    }
+    // Matrix with each row being a data point, last column is the class it belongs to.
+    weka.core.matrix.Matrix matrix = new weka.core.matrix.Matrix(double_matrix);
+    if (DEBUG) {
+      System.out.println("Matrix was constructed as:");
+      System.out.println("first columns are attributes values, and last column is class number:\n" + matrix);
     }
     return result;
   }
 
   public static void main(String[] args) {
-    runFilter(new SimpleBatch(), args);
+    runFilter(new HDA(), args);
   }
 }
